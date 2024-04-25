356677eceda53c9bb6efb9884d313f4e9380c4aa
Leonardo Fujimura <leonardo.fujimura123@gmail.com>
2024-04-25 20:01:50 -0300

    omnitrix animations and rotary encoder

--- Ominitrix.ino
+++ Ominitrix.ino
@@ -1,8 +1,9 @@
-#include <AnimatedGIF.h>
+#include <PNGdec.h>
 #include <TFT_eSPI.h>
-#include "aliens.h"
+#include "SPI.h"
+#include "omnitrix_aliens.h"
+#include "omnitrix_alien_backround.h"
 #include "omnitrixAnimation.h"
-#include "teste2.h"
 
 /*
     Arduino -> UNO
@@ -13,7 +14,7 @@
     |     VCC     |  3.3V   |
     |     GND     |  GROUND |
     |     SCL     |  13     |
-    |     SDA     |  12     | 
+    |     SDA     |  12     |
     |     DC      |  11     |
     |     CS      |  9      |
     |     RST     |  10     |
@@ -29,46 +30,56 @@
     |     VCC     |  3.3V   |
     |     GND     |  GROUND |
     |     SCL     |  18     |
-    |     SDA     |  13     |    
+    |     SDA     |  13     |
     |     DC      |  2      |
     |     CS      |  9      |
     |     RST     |  4      |
     |_____________|_________|
 */
 
-#define potentiometer A1
-#define btnAlienChooser 5
-#define buzzer 4
-#define btnActivate 6
+#define buzzer 12
+#define potentiometer 13
+#define btnAlienChooser 7
+#define btnActivate 17
 
-#define TFT_CS 9
-#define TFT_DC 2
-// Adafruit_GC9A01A tft = Adafruit_GC9A01A(TFT_CS, TFT_DC);
+
+#define ROTARY_PINCLK 39
+#define ROTARY_PINDT 40
+
+volatile bool rotaryEncoder = false;
+
+void IRAM_ATTR rotary() {
+  rotaryEncoder = true;
+}
 
 TFT_eSPI tft = TFT_eSPI();
-AnimatedGIF gif;
+int16_t xpos = 0;
+int16_t ypos = 0;
+
+PNG png;
 
 #define DISPLAY_WIDTH 240
 #define DISPLAY_HEIGHT 240
 #define IMAGE_WIDTH 100
 #define IMAGE_HEIGHT 120
+#define MAX_IMAGE_WIDTH 240
 #define GIF_IMAGE omniStartGIF
 
 boolean isAlienForm = false;
 boolean isActivate = false;
 boolean isSameRecharge = false;
-boolean hasLastPotentiometer = false;
-byte lastPotentiometerValue = 0;
+boolean hasLastAlien = false;
+int lastAlienValue = 0;
 long elapsedTime = 0L;
 long morphedTime = 0L;
 long minMorphTime = 5000L;
 long deschargeTime = 15000L;
 long batteryValue = deschargeTime;
 long loopStart;
+int alienNo = 0;
 
-void setup()
-{
-  Serial.begin(9600);
+void setup() {
+  Serial.begin(115200);
 
   tft.begin();
   tone(buzzer, 800);
@@ -79,49 +90,34 @@ void setup()
   delay(100);
   noTone(buzzer);
 
-  byte oo =0;
-
-  while(oo<10){
-    Serial.println("ligado");
-    delay(1000);
-  }
-
-  tft.fillScreen(0x0000);
-  gif.begin(BIG_ENDIAN_PIXELS);
-
-  // tft.drawRGBBitmap();
-
-  fadeInScreen(0x0F00);
-
-  // for (byte i = 0; i < omnitrixAnimationLength; i++) {
-  //   tft.drawBitmap((DISPLAY_WIDTH - 240) / 2, (DISPLAY_HEIGHT - 240) / 2, omnitrixAnimation[i], DISPLAY_WIDTH, DISPLAY_HEIGHT, 0xFFFF);
-  //   tft.fillScreen(0x0000);
-  // }
   pinMode(potentiometer, INPUT);
-  pinMode(btnAlienChooser, INPUT);
+  pinMode(btnAlienChooser, INPUT_PULLUP);
   pinMode(btnActivate, INPUT);
   pinMode(buzzer, OUTPUT);
-  pinMode(15, OUTPUT);
-  digitalWrite(15,HIGH);
+
+  pinMode(ROTARY_PINCLK, INPUT);
+  pinMode(ROTARY_PINDT, INPUT);
+
+  attachInterrupt(digitalPinToInterrupt(ROTARY_PINCLK), rotary, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ROTARY_PINDT), rotary, CHANGE);
+
+  tft.fillScreen(0x12319721);
+  int16_t rc = png.openFLASH((uint8_t *)omnitrix_anim[0], sizeof(omnitrix_anim[0]), pngDraw);
+
+  if (rc == PNG_SUCCESS) {
+    tft.startWrite();
+    rc = png.decode(NULL, 0);
+    tft.endWrite();
+    tft.endWrite();
+
+    // png.close(); // Required for files, not needed for FLASH arrays
+  }
 }
 
-void loop()
-{
-  if (gif.open((uint8_t *)GIF_IMAGE, sizeof(GIF_IMAGE), GIFDraw))
-  {
-    tft.startWrite();
-    while (gif.playFrame(true, NULL))
-    {
-      yield();
-    }
-    gif.close();
-    tft.endWrite();
-  }
-
+void loop() {
   loopStart = millis();
 
-  if (digitalRead(btnActivate) == HIGH)
-  {
+  if (digitalRead(btnActivate) == HIGH) {
     // travado enquanto o botão estiver segurado
     while (digitalRead(btnActivate) == HIGH)
       ;
@@ -131,11 +127,9 @@ void loop()
   omnitrixRecharge();
 }
 
-void omnitrixModeSet()
-{
+void omnitrixModeSet() {
   // se desligado e com bateria maior q 0
-  if (isActivate == false && batteryValue > minMorphTime)
-  {
+  if (isActivate == false && batteryValue > minMorphTime) {
     // liga o omnitrix
     isActivate = true;
 
@@ -143,8 +137,7 @@ void omnitrixModeSet()
     alienSelect();
   }
   // se desligado e sem bateria
-  else if (isActivate == false && batteryValue < minMorphTime)
-  {
+  else if (isActivate == false && batteryValue < minMorphTime) {
     // toca o som
     tone(buzzer, 125);
     delay(250);
@@ -152,9 +145,7 @@ void omnitrixModeSet()
     delay(100);
 
     // se ligado
-  }
-  else if (isActivate == true)
-  {
+  } else if (isActivate == true) {
     // desliga o omnitrix
     isActivate = false;
     omnitrixShutdown();
@@ -162,8 +153,18 @@ void omnitrixModeSet()
   }
 }
 
-void omnitrixStartup()
-{
+void omnitrixStartup() {
+
+  int16_t rc = png.openFLASH((uint8_t *)omnitrix_anim[0], sizeof(omnitrix_anim[0]), pngDraw);
+
+  if (rc == PNG_SUCCESS) {
+    tft.startWrite();
+    rc = png.decode(NULL, 0);
+    tft.endWrite();
+    tft.endWrite();
+
+    // png.close(); // Required for files, not needed for FLASH arrays
+  }
   // toca o som
   tone(buzzer, 1000);
   delay(100);
@@ -175,14 +176,51 @@ void omnitrixStartup()
   delay(100);
 }
 
-void alienSelect()
-{
-  fadeInScreen(0x0000);
-  while (isActivate == true)
-  {
+void getAlienNo() {
+  if (rotaryEncoder) {
+    // Get the movement (if valid)
+    int8_t rotationValue = checkRotaryEncoder();
 
-    if (digitalRead(btnActivate) == HIGH)
-    {
+    // If valid movement, do something
+    if (rotationValue != 0) {
+      alienNo += rotationValue;
+      if (alienNo < 0) {
+        alienNo = alienCount - 1;
+      }
+      if (alienNo >= alienCount) {
+        alienNo = 0;
+      }
+    }
+  }
+}
+
+void alienSelect() {
+  tft.fillScreen(0x12319721);
+
+  int frame = 0;
+  int fps = 10;
+
+  for (; frame <= omnitrix_anim_N; frame++) {
+
+    int16_t rc = png.openFLASH((uint8_t *)omnitrix_anim[frame], sizeof(omnitrix_anim[frame]), pngDraw);
+
+    if (rc == PNG_SUCCESS) {
+      tft.startWrite();
+      rc = png.decode(NULL, 0);
+      tft.endWrite();
+      tft.endWrite();
+
+      // png.close(); // Required for files, not needed for FLASH arrays
+    }
+    delay(fps);
+  }
+  delay(100);
+
+  changeAlien(lastAlienValue);
+
+  while (isActivate == true) {
+
+    if (digitalRead(btnActivate) == HIGH) {
       while (digitalRead(btnActivate) == HIGH)
         ;
       isActivate = false;
@@ -190,28 +228,29 @@ void alienSelect()
       return;
     }
 
-    byte alienNo = map(analogRead(potentiometer), 0, 650, 0, 9);
+    getAlienNo();
 
-    if (hasLastPotentiometer == false)
-    {
-      changeAlien(lastPotentiometerValue, alienNo);
-      hasLastPotentiometer = true;
-      lastPotentiometerValue = alienNo;
+    if (hasLastAlien == false) {
+      changeAlien(alienNo);
+      hasLastAlien = true;
+      lastAlienValue = alienNo;
     }
 
-    if (alienNo != lastPotentiometerValue)
-    {
+    if (alienNo != lastAlienValue) {
+      lastAlienValue = alienNo;
       tone(buzzer, 125);
       delay(100);
       noTone(buzzer);
-      changeAlien(lastPotentiometerValue, alienNo);
-      lastPotentiometerValue = alienNo;
+      changeAlien(alienNo);
     }
 
-    if (digitalRead(btnAlienChooser) == HIGH)
-    {
 
-      hasLastPotentiometer = false;
+
+    if (digitalRead(btnAlienChooser) == LOW) {
+      while (digitalRead(btnAlienChooser) == LOW)
+        ;
+
+      hasLastAlien = false;
       isActivate = false;
       isAlienForm = true;
       isSameRecharge = false;
@@ -221,15 +260,12 @@ void alienSelect()
       fadeInScreen(0xFFFF);
 
       long morphStart = millis();
-      while (isAlienForm)
-      {
+      while (isAlienForm) {
         morphedTime = millis() - morphStart;
-        if (morphedTime >= batteryValue)
-        {
+        if (morphedTime >= batteryValue) {
           batteryValue -= morphedTime;
 
-          if (batteryValue < 0)
-          {
+          if (batteryValue < 0) {
             batteryValue = 0;
           }
 
@@ -238,14 +274,12 @@ void alienSelect()
           loopStart = millis();
         }
 
-        if (digitalRead(btnActivate) == HIGH && morphedTime >= minMorphTime)
-        {
+        if (digitalRead(btnActivate) == HIGH && morphedTime >= minMorphTime) {
           while (digitalRead(btnActivate) == HIGH)
             ;
           batteryValue -= morphedTime;
 
-          if (batteryValue < 0)
-          {
+          if (batteryValue < 0) {
             batteryValue = 0;
           }
 
@@ -258,41 +292,54 @@ void alienSelect()
   }
 }
 
-void changeAlien(byte lastAlienNo, byte alienNo)
-{
-  if (lastAlienNo > alienNo)
-  {
-    for (int x = (DISPLAY_WIDTH - IMAGE_WIDTH) / 2; x <= (DISPLAY_WIDTH - IMAGE_WIDTH) / 2 + IMAGE_WIDTH; x++)
-    {
-      tft.drawFastVLine(x, (DISPLAY_HEIGHT - IMAGE_HEIGHT) / 2, IMAGE_HEIGHT, 0x0000);
-    }
-  }
-  else
-  {
-    for (int x = (DISPLAY_WIDTH - IMAGE_WIDTH) / 2 + IMAGE_WIDTH; x >= (DISPLAY_WIDTH - IMAGE_WIDTH) / 2; x--)
-    {
-      tft.drawFastVLine(x, (DISPLAY_HEIGHT - IMAGE_HEIGHT) / 2, IMAGE_HEIGHT, 0x0000);
-    }
+void changeAlien(int alienNo) {
+
+  int16_t rc = png.openFLASH((uint8_t *)omnitrix_alien_backround, sizeof(omnitrix_alien_backround), pngDraw);
+
+  if (rc == PNG_SUCCESS) {
+    tft.startWrite();
+    rc = png.decode(NULL, 0);
+    tft.endWrite();
+    tft.endWrite();
+
+    // png.close(); // Required for files, not needed for FLASH arrays
   }
 
-  tft.drawBitmap((DISPLAY_WIDTH - IMAGE_WIDTH) / 2, (DISPLAY_HEIGHT - IMAGE_HEIGHT) / 2, alienList[alienNo], IMAGE_WIDTH, IMAGE_HEIGHT, 0xFFFF);
+  delay(100);
+
+  rc = png.openFLASH((uint8_t *)omnitrix_aliens[alienNo], sizeof(omnitrix_aliens[alienNo]), pngDraw);
+
+  if (rc == PNG_SUCCESS) {
+    tft.startWrite();
+    rc = png.decode(NULL, 0);
+    tft.endWrite();
+    tft.endWrite();
+
+    // png.close(); // Required for files, not needed for FLASH arrays
+  }
 }
 
-void omnitrixShutdown()
-{
+void omnitrixShutdown() {
 
   // toca o som
   tone(buzzer, 250);
   delay(100);
   noTone(buzzer);
-  fadeInScreen(0x0F00);
-  hasLastPotentiometer = false;
+  tft.fillScreen(0x12319721);
+  int16_t rc = png.openFLASH((uint8_t *)omnitrix_anim[0], sizeof(omnitrix_anim[0]), pngDraw);
+
+  if (rc == PNG_SUCCESS) {
+    tft.startWrite();
+    rc = png.decode(NULL, 0);
+    tft.endWrite();
+    tft.endWrite();
+
+    // png.close(); // Required for files, not needed for FLASH arrays
+  }
 }
 
-void omnitrixDescharge()
-{
+void omnitrixDescharge() {
   isAlienForm = false;
-  hasLastPotentiometer = false;
 
   // vermelho
   fadeInScreen(0xF000);
@@ -320,16 +367,13 @@ void omnitrixDescharge()
   fadeInScreen(0xF000);
 }
 
-void omnitrixRecharge()
-{
-  if (batteryValue < deschargeTime)
-  {
+void omnitrixRecharge() {
+  if (batteryValue < deschargeTime) {
     delay(100);
     elapsedTime += millis() - loopStart;
     batteryValue += elapsedTime;
 
-    if (batteryValue >= minMorphTime && isSameRecharge == false)
-    {
+    if (batteryValue >= minMorphTime && isSameRecharge == false) {
       morphedTime = 0L;
       tone(buzzer, 800);
       delay(100);
@@ -342,8 +386,7 @@ void omnitrixRecharge()
       fadeInScreen(0x0F00);
     }
 
-    if (batteryValue >= deschargeTime)
-    {
+    if (batteryValue >= deschargeTime) {
       batteryValue = deschargeTime;
       tone(buzzer, 800);
       delay(100);
@@ -362,11 +405,56 @@ void omnitrixRecharge()
   }
 }
 
-void fadeInScreen(uint16_t color)
-{
-  int i = 1;
-  while (i <= DISPLAY_WIDTH / 2 + 1)
-  {
-    tft.fillCircle(DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2, i += 10, color);
+void fadeInScreen(uint16_t color) {
+  tft.fillCircle(DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2, DISPLAY_WIDTH / 2 + 1, color);
+  int16_t rc = png.openFLASH((uint8_t *)omnitrix_anim[0], sizeof(omnitrix_anim[0]), pngDraw);
+
+  if (rc == PNG_SUCCESS) {
+    tft.startWrite();
+    rc = png.decode(NULL, 0);
+    tft.endWrite();
+    tft.endWrite();
+
+    // png.close(); // Required for files, not needed for FLASH arrays
   }
+}
+
+int8_t checkRotaryEncoder() {
+  // Reset the flag that brought us here (from ISR)
+  rotaryEncoder = false;
+
+  static uint8_t lrmem = 3;
+  static int lrsum = 0;
+  static int8_t TRANS[] = { 0, -1, 1, 14, 1, 0, 14, -1, -1, 14, 0, 1, 14, 1, -1, 0 };
+
+  // Read BOTH pin states to deterimine validity of rotation (ie not just switch bounce)
+  int8_t l = digitalRead(ROTARY_PINCLK);
+  int8_t r = digitalRead(ROTARY_PINDT);
+
+  // Move previous value 2 bits to the left and add in our new values
+  lrmem = ((lrmem & 0x03) << 2) + 2 * l + r;
+
+  // Convert the bit pattern to a movement indicator (14 = impossible, ie switch bounce)
+  lrsum += TRANS[lrmem];
+
+  /* encoder not in the neutral (detent) state */
+  if (lrsum % 4 != 0) {
+    return 0;
+  }
+
+  /* encoder in the neutral state - clockwise rotation*/
+  if (lrsum == 4) {
+    lrsum = 0;
+    return 1;
+  }
+
+  /* encoder in the neutral state - anti-clockwise rotation*/
+  if (lrsum == -4) {
+    lrsum = 0;
+    return -1;
+  }
+
+  // An impossible rotation has been detected - ignore the movement
+  lrsum = 0;
+  return 0;
 }
